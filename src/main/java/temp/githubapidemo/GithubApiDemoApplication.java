c3cc103c0dc135bd46d197ff87dd49280831ccf2
Jakub Krezolek <kuborek2@gmail.com>
2023-06-06 14:32:33 +0200

    Inital commit

---
+++ src/main/java/temp/githubapidemo/GithubApiDemoApplication.java
@@ -0,0 +1,13 @@
+package temp.githubapidemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubApiDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubApiDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/temp/githubapidemo/GithubApiDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package temp.githubapidemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubApiDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
